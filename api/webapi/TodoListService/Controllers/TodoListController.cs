a5f35afb6b8093de60882e678256749fee179317
Noemi Z. Veneracion (MSA) <noemi616@live.com>
2019-09-20 16:25:01 +1000

    first commit

---
+++ api/webapi/TodoListService/Controllers/TodoListController.cs
@@ -0,0 +1,84 @@
+﻿/*
+ The MIT License (MIT)
+
+Copyright (c) 2018 Microsoft Corporation
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Web.Resource;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using TodoListService.Models;
+
+namespace TodoListService.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    public class TodoListController : Controller
+    {
+        ConcurrentBag<TodoItem> TodoStore = new ConcurrentBag<TodoItem>();
+
+        /// <summary>
+        /// The Web API will only accept tokens 1) for users, 2) having the user_impersonation scope for
+        /// this API (if you created the app using the App creation script) or 'access_as_user' if you created
+        /// it following the README.md.
+        /// </summary>
+        static string[] scopeRequiredByAPI = new string[] { "user_impersonation", "access_as_user" };
+
+        public TodoListController()
+        {
+            
+
+        }
+
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<TodoItem> Get()
+        {
+            HttpContext.VerifyUserHasAnyAcceptedScope(scopeRequiredByAPI);
+            string owner = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            populateStore();
+            return TodoStore.ToList();
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]TodoItem todo)
+        {
+            HttpContext.VerifyUserHasAnyAcceptedScope(scopeRequiredByAPI);
+            string owner = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            TodoStore.Add(new TodoItem { Owner = owner, Title = todo.Title });
+        }
+
+        private void populateStore()
+        {
+            if (TodoStore.Count == 0)
+            {
+                TodoStore.Add(new TodoItem { Owner = "Owner1", Title = "Title1" });
+                TodoStore.Add(new TodoItem { Owner = "Owner2", Title = "Title2" });
+                TodoStore.Add(new TodoItem { Owner = "Owner3", Title = "Title3" });
+            }
+        }
+    }
+}
---
+++ api/webapi/TodoListService/Startup.cs
@@ -0,0 +1,90 @@
+﻿/*
+ The MIT License (MIT)
+
+Copyright (c) 2018 Microsoft Corporation
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Identity.Web;
+
+namespace TodoListService
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //Adding CorsPolicy from https://weblog.west-wind.com/posts/2016/sep/26/aspnet-core-and-cors-gotchas
+            //and https://www.c-sharpcorner.com/article/enabling-cors-in-asp-net-core-api-application/
+            services.AddCors(c =>
+            {
+                c.AddPolicy("CORSPolicy", options => options.AllowAnyOrigin()
+                                                            .AllowAnyMethod()
+                                                            .AllowAnyHeader()
+                                                            .AllowCredentials());
+            });
+
+            services.AddProtectedWebApi(Configuration);
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            //Adding CorsPolicy from https://weblog.west-wind.com/posts/2016/sep/26/aspnet-core-and-cors-gotchas
+            //and https://www.c-sharpcorner.com/article/enabling-cors-in-asp-net-core-api-application/
+            app.UseCors("CORSPolicy");
+
+            //app.UseCors(options => options.WithOrigins("http://localhost:30662", "https://localhost:30662"));
+
+            if (env.IsDevelopment())
+            {
+                // Since IdentityModel version 5.2.1 (or since Microsoft.AspNetCore.Authentication.JwtBearer version 2.2.0),
+                // PII hiding in log files is enabled by default for GDPR concerns.
+                // For debugging/development purposes, one can enable additional detail in exceptions by setting IdentityModelEventSource.ShowPII to true.
+                // Microsoft.IdentityModel.Logging.IdentityModelEventSource.ShowPII = true;
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+           
+
+            app.UseHttpsRedirection();
+            app.UseAuthentication();
+            app.UseMvc();
+        }
+    }
+}
